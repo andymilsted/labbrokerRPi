6344a000a9b97f7f0dfee0da1f408735cb4987f1
Andrew Milsted <andrew@green.chem.soton.ac.uk>
2013-10-30 16:53:20 +0000

    startup part 2

--- bin/startUp.php
+++ bin/startUp.php
@@ -6,3 +6,8 @@ include("../lib/labbrokerRPi.php");
 if(isset($config['ddns'])){
 	file_get_contents($config['ddns']);
 }
+
+if(isset($config['labbroker']['DHT']) && count($config['labbroker']['DHT'])){
+	`modprobe w1-gpio`;	
+	`modprobe w1-therm`;
+}
