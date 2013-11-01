e72c323e8340c56b7c45c58f01eb8e44dd7988ae
Andrew Milsted <andrew@green.chem.soton.ac.uk>
2013-11-01 12:03:17 +0000

    added init to rc.d

--- bin/install.php
+++ bin/install.php
@@ -10,9 +10,10 @@ include("../lib/labbrokerRPi.php");
 #Init.d
 
 $init = file_get_contents("../lib/install/initd-labbrokerRPi");
-
 $init = str_replace("__DIR__", $config['pwd']."/bin", $init);
-
 file_put_contents("/etc/init.d/labbrokerRPi", $init);
-
 chmod("/etc/init.d/labbrokerRPi", 0755);
+`sudo update-rc.d labbrokerRPi defaults`;
+
+#make cron
+`php {$config['pwd']}/bin/makeCron.php`;
