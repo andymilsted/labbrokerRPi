0798830b283e71174181d1e17739b7086bf89a98
Andrew Milsted <andrew@green.chem.soton.ac.uk>
2013-11-01 11:59:14 +0000

    make executable

--- bin/install.php
+++ bin/install.php
@@ -14,3 +14,5 @@ $init = file_get_contents("../lib/install/initd-labbrokerRPi");
 $init = str_replace("__DIR__", $config['pwd']."/bin", $init);
 
 file_put_contents("/etc/init.d/labbrokerRPi", $init);
+
+chmod("/etc/init.d/labbrokerRPi", 0755);
